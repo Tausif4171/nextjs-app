59230a23bf32de4484cd3be0aed3bf21e1a054ef
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-14 23:34:13 +0530

    Routing ✅

---
+++ app/users/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UsersList = () => {
+    return (
+        <div className='text-[#000000] p-6'>UsersList</div>
+    )
+}
+
+export default UsersList
---
+++ app/users/user/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const User = () => {
+    return (
+        <div className='text-[#000000] p-6'>User</div>
+    )
+}
+
+export default User
