5ad0f72d590500b84fa17d21dfe4ee25b193ce07
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-14 23:40:06 +0530

    Navigation ✅

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex text-[22px] text-[#000000] font-semibold min-h-screen flex-col items-center justify-between p-24">
-      NextJS Learning! 🎉
+    <main className="p-10">
+      <h1 className="flex text-[22px] text-[#000000] font-semibold">NextJS Learning! 🎉</h1>
+      <Link href='/users'><p className="flex text-[18px] text-[#000000]">Users List</p></Link>
     </main>
   );
 }
