f4349747f5cd896b407da7dfbca91593baea75bc
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-16 23:59:48 +0530

    data fetching ✅

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const UsersList = () => {
+interface IUser {
+    userId: number,
+    title: string,
+}
+
+const UsersList = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/1/posts')
+    const users: IUser[] = await res.json()
     return (
-        <div className='text-[#000000] p-6'>UsersList</div>
+        <div>
+            <p className='text-[#000000] p-6'>UsersList</p>
+            <ul>
+                {users.map((user) => <li className="flex text-[22px] text-[#000000] font-semibold" key={user.userId}>{user.title}</li>)}
+            </ul>
+        </div>
     )
 }
 
