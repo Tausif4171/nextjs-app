673cd1c9e68e5251b6913cb03e12c982ac260b11
Tausif Khan <tausifkhan4171@gmail.com>
2024-04-16 23:28:46 +0530

    client component

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+function ProductCard() {
+    return (
+        <div>
+            <button className="flex text-[22px] text-[#000000] font-semibold" onClick={() => console.log('test')}>Add to cart</button>
+        </div>
+    )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main className="p-10">
       <h1 className="flex text-[22px] text-[#000000] font-semibold">NextJS Learning! 🎉</h1>
       <Link href='/users'><p className="flex text-[18px] text-[#000000]">Users List</p></Link>
+      <ProductCard />
     </main>
   );
 }
